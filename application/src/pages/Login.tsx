54ebe0604d8b3c8cac7eef93356aa1e4952f5666
Tola Leng <tolaleng.oss@gmail.com>
2025-05-12 23:23:40 +0800

    Updated Login page (Removed unnecessary and unsued button)

--- application/src/pages/Login.tsx
+++ application/src/pages/Login.tsx
@@ -79,11 +79,7 @@ const Login = () => {
         <div className="text-center relative">
           <div className="absolute right-0 top-0">
             <Popover>
-              <PopoverTrigger asChild>
-                <Button variant="outline" size="icon" aria-label="API Settings">
-                  <Settings className="h-4 w-4" />
-                </Button>
-              </PopoverTrigger>
+
             {/*   <PopoverContent className="w-[260px] sm:w-80">   //this allow test api connection in login page
                 <div className="space-y-4">
                   <h4 className="font-medium">API Endpoint Settings</h4>
@@ -109,38 +105,10 @@ const Login = () => {
             </Popover>
           </div>
           <h1 className="text-xl sm:text-2xl font-bold tracking-tight text-foreground">{t("signInToYourAccount")}</h1>
-          <p className="mt-2 text-xs sm:text-sm text-muted-foreground">
-            {t("dontHaveAccount")} <a href="#" className="font-medium text-emerald-500 hover:text-emerald-400">{t("createOne")}</a>
-          </p>
+
         </div>
 
         <div>
-          <Button
-            variant="outline"
-            className="w-full py-4 sm:py-6 flex items-center justify-center gap-2 bg-background"
-            disabled={loading}
-          >
-            <svg className="w-4 h-4 sm:w-5 sm:h-5" viewBox="0 0 24 24">
-              <path
-                d="M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92c-.26 1.37-1.04 2.53-2.21 3.31v2.77h3.57c2.08-1.92 3.28-4.74 3.28-8.09z"
-                fill="#4285F4"
-              />
-              <path
-                d="M12 23c2.97 0 5.46-.98 7.28-2.66l-3.57-2.77c-.98.66-2.23 1.06-3.71 1.06-2.86 0-5.29-1.93-6.16-4.53H2.18v2.84C3.99 20.53 7.7 23 12 23z"
-                fill="#34A853"
-              />
-              <path
-                d="M5.84 14.09c-.22-.66-.35-1.36-.35-2.09s.13-1.43.35-2.09V7.07H2.18C1.43 8.55 1 10.22 1 12s.43 3.45 1.18 4.93l2.85-2.22.81-.62z"
-                fill="#FBBC05"
-              />
-              <path
-                d="M12 5.38c1.62 0 3.06.56 4.21 1.64l3.15-3.15C17.45 2.09 14.97 1 12 1 7.7 1 3.99 3.47 2.18 7.07l3.66 2.84c.87-2.6 3.3-4.53 6.16-4.53z"
-                fill="#EA4335"
-              />
-            </svg>
-            <span className="text-sm sm:text-base">{t("signInWithGoogle")}</span>
-          </Button>
-
           <div className="mt-4 sm:mt-6 relative">
             <div className="absolute inset-0 flex items-center">
               <div className="w-full border-t border-border"></div>
